f878f4a98463f7d59f7caea74441738e0eff2d1f
ATOM03 <priyanshujhaginia@outlook.com>
2020-08-12 21:45:16 +0530

    Updates

---
+++ precache-manifest.9944c04116d2abefb6f0da7ef45d0079.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc63f4e2c74a41274e2cb21c005b82d0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "68bbc042195ffaa990a2",
+    "url": "/Portfolio/static/css/main.86edce24.chunk.css"
+  },
+  {
+    "revision": "23c06b59c07e84c4799d",
+    "url": "/Portfolio/static/js/2.3118b058.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.3118b058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68bbc042195ffaa990a2",
+    "url": "/Portfolio/static/js/main.a3159084.chunk.js"
+  },
+  {
+    "revision": "97cf07384dbf9433c508",
+    "url": "/Portfolio/static/js/runtime-main.feb05c9b.js"
+  },
+  {
+    "revision": "84d041cca7f409fc51ec497aba71cadc",
+    "url": "/Portfolio/static/media/549683-black-3d-background-1920x1200-samsung.84d041cc.jpg"
+  },
+  {
+    "revision": "1cf5d8befbeb89f3a1b8562d22ce3cf0",
+    "url": "/Portfolio/static/media/Resume.1cf5d8be.pdf"
+  },
+  {
+    "revision": "7240ec62273a3d8f2f253b7d1a8afe1e",
+    "url": "/Portfolio/static/media/Screenshot from 2020-07-27 19-28-44.7240ec62.png"
+  },
+  {
+    "revision": "33b673904a9e7af3212bb1c3346c1121",
+    "url": "/Portfolio/static/media/Screenshot from 2020-07-27 20-59-48.33b67390.png"
+  }
+]);
